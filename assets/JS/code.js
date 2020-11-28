1f8771bc047c196f8f5659e9831f99b9dd41fe64
Levi <levibosando5@gmail.com>
2020-11-28 11:47:56 +0300

    Initial commit

---
+++ assets/JS/code.js
@@ -0,0 +1,11 @@
+/* 
+Dropdownicon function
+*/
+function dropdownMenu() {
+    var x = document.getElementById("dropdownClick");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
